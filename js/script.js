e4a0c4403f83e9b33eeec0cbce3c140702da0c85
GustavoCMarques <gu2104.marques@gmail.com>
2023-05-05 09:57:40 -0300

    js

---
+++ js/script.js
@@ -0,0 +1,58 @@
+ /*<script> </script>
+    // Declarar uma variável com a palavra reservada var
+
+    var nr1 = 10;
+    var nr2 = 5;
+    var resultado;
+
+    resultado = !(parseInt(nr1)%parseInt(nr2)) ? "PAR" : "IMPAR";
+    console.log(`O NÚMERO É : ${resultado}`);
+    
+    
+    //Imprimir as variáveis com o objeto console.
+
+    console.log(nr1);
+    console.log(nr2);
+
+    //Realizando as operações básicas com estes valores
+
+    // + / - / * e %
+
+    //Adição
+
+    console.log("Resultado : " + (nr1 + nr2));
+    console.log("Resultado : " + (parseInt(nr1) + parseInt(nr2)));
+    console.log(`"Resultado : " $(parseInt(nr1) + parseInt(nr2))`);
+    //SUBTRAÇÃO
+
+    console.log("Resultado: " + (nr1 - nr2)) 
+
+    //MULTIPLICAÇÃO
+
+    console.log("Resultado: " + (nr1 * nr2))
+
+    //DIVISÃO
+
+    console.log("Resultado: " + (nr1 / nr2))
+
+    //OBTENHA O RESTO DA DIVISÃO
+
+    console.log("Resultado: " + (nr1 % nr2))
+
+*/
+ 
+
+//var nome1 = "Beatriz";
+//let nome2 = "Matheus";
+
+var nome = "Beatriz";
+
+if(nome != ""){
+    var nome = "Robson";
+}
+
+//Regras Const
+//1ª Regra = Inicializar na declaração
+const nome3 = "Violante";
+//2ª Regra = Não pode ser alterado durante o fluxo do programa.
+nome3 = "Jonathan";
