8ef8537dfcd2b0b6284afb0c432b4008d1870894
GustavCMarques <gu2104.marques@gmail.com>
2023-05-12 09:58:22 -0300

    Java script da calculadora

--- js/calc.js
+++ js/calc.js
@@ -1,11 +1,36 @@
 
 function insereNumeroNoVisor(botao,visor) {
-    visor.value += botao.value;
+ 
+    if(botao.value == "+" || botao.value == "-" || botao.value == "*" || botao.value == "/"){
+
+        if(visor.value[visor.value.length-1] == "+" || visor.value[visor.value.length-1] == "-"
+        || visor.value[visor.value.length-1] == "*" || visor.value[visor.value.length-1] == "/"){
+            visor.value.slice(1);
+            visor.value += botao.value;
+        }else{
+            visor.value += botao.value;
+        }
+
+    }else{
+        visor.value += botao.value;
+    }
 
 }
 
 const resultadoDasOperacoes = (visor) =>{
 
-    visor.value = eval(visor.value);
+    try {
+        //Utilizando a função eva(expressão)
+        visor. value = eval(visor.value);
+    } catch(error) {
+            if(visor.value[visor.value.length-1] == "+" || visor.value[visor.value.length-1] == "-"
+            || visor.value[visor.value.length-1] == "*" || visor.value[visor.value.length-1] == "/"){
+                visor.value = visor.value.slice(1);
+            }else{
+                visor.value.shift();
+            }
+        }
+
 
 }
+
